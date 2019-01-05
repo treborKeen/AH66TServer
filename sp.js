c23a066fc2d36b407bbf24232919857b87320829
Demo User <debian@beaglebone.localdomain>
2019-01-05 19:46:46 +0000

    minor changes?

--- sp.js
+++ sp.js
@@ -1,6 +1,7 @@
 //ls /sys/devices/platform/ocp/ | grep pinmux*
 //config-pin 'P9.11' uart
 //config-pin 'P9.13' uart
+// cat /sys/devices/platform/ocp/ocp:P9_11_pinmux/state
 //var fs = require('fs');
 
 var b = require('bonescript');
@@ -33,7 +34,9 @@ function start(ser) {
     parser.on('data', processData);
     setInterval(function() {
       b.digitalRead('P8_7', function(err, x) {
-        if (err) throw err;
+        if (err) {
+          throw err;
+        }
         //console.log("read pin 8_7: "+x);
         if (x && !doorbellOn) {
           doorbellOn = 1;
@@ -57,12 +60,12 @@ function start(ser) {
     }
 
     port.write('&AH66,R1,TUNE,?\r');
-    //sp.write('&AH66,R2,TUNE,?\r')
+    //port.write('&AH66,R2,TUNE,?\r')
 
     setTimeout(function() {
 
-      console.log('tuners are :' + tuners);
-      console.log('zone data :' + zData);
+      //console.log('tuners are :' + tuners);
+      //console.log('zone data :' + zData);
       //console.log('zData 1,1 :  '+zData[1][1]);
       socket.emit('update', tuners, zData, curZone, mdf);
     }, 500);
@@ -168,7 +171,7 @@ function processData(data) {
 } //end processData
 
 function playDoorbell() {
-  console.log('playing doorbell ')
+  console.log('playing doorbell*************************************');
   console.log((new Date()).toLocaleString("en-US", {timeZone: "America/Chicago"}));
   port.write("&AH66,DB,1\r");
   /*setTimeout(function(){
@@ -178,7 +181,7 @@ function playDoorbell() {
 }
 
 function stopDoorbell() {
-  console.log('stopping doorbell');
+  console.log('stopping doorbell*************************************');
   port.write("&AH66,DB,0\r");
 }
 
